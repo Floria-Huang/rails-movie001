8a87272633cad94eb929d9f5da0ac56924f14bfc
Floria <yao0406@126.com>
2017-10-01 17:30:37 +0800

    join & quit movie action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
@@ -41,6 +41,33 @@ def destroy
   flash[:alert] = "Movie deleted"
   redirect_to movies_path
 end
+
+
+def join
+   @movie = Movie.find(params[:id])
+
+    if !current_user.is_member_of?(@movie)
+      current_user.join!(@movie)
+      flash[:notice] = "收藏本电影成功！"
+    else
+      flash[:warning] = "你已收藏过本电影！"
+    end
+
+    redirect_to movie_path(@movie)
+  end
+
+def quit
+    @movie = Movie.find(params[:id])
+
+    if current_user.is_member_of?(@movie)
+      current_user.quit!(@movie)
+      flash[:alert] = "已取消收藏本电影！"
+    else
+      flash[:warning] = "你尚未收藏本电影，怎么取消 XD"
+    end
+
+    redirect_to movie_path(@movie)
+  end
 
 private
 def find_movie_and_check_permission
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :movies do
+
+member do
+  post :join
+  post :quit
+end
+
     resources :critics
   end
 root 'movies#index'
